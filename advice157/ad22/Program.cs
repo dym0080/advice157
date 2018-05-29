9b7b4d114b847df4a3268fd5143527ebb4cbe490
dym <308960474@qq.com>
2018-05-29 20:49:53 +0800

    add

---
+++ advice157/ad22/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ad22
+{
+    class Program
+    {
+        static List<Person> list = new List<Person>()
+        {
+            new Person() {Name="Rose",Age=17 },
+            new Person() {Name="Mike",Age=19 },
+            new Person() {Name="Lily",Age=20 }
+        };
+
+        static AutoResetEvent autoSet = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() =>
+              {
+                  // 确保等待t2开始之后才运行下面的代码
+                  autoSet.WaitOne();
+                  foreach(var item in list)
+                  {
+                      Console.WriteLine("t1:" + item.Name);
+                      Thread.Sleep(5000);
+                  }
+              });
+            t1.Start();
+
+            Thread t2 = new Thread(() =>
+              {
+                  // 通知t1可以执行代码
+                  autoSet.Set();
+                  // 沉睡1秒后是为了确保删除操作在t1的迭代过程中
+                  Thread.Sleep(5000);
+                  list.RemoveAt(2);
+              });
+            t2.Start();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
--- advice157/advice157/Program.cs
+++ advice157/advice157/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,9 @@ namespace advice157
     {
         static void Main(string[] args)
         {
-        }
+            int[] arr = { 0,1,2,3,4,5,6};
+            ArrayList list1 = ArrayList.Adapter(arr);
+
+            List<int> list2 = arr.ToList<int>();
     }
 }
